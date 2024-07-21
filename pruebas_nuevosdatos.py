9100d4510e8735c8aed37390fe4bc202cf9e1bd2
AndresGarcia-15 <teherana@utb.edu.co>
2024-07-21 11:52:45 -0500

    Update final

--- pruebas_nuevosdatos.py
+++ pruebas_nuevosdatos.py
@@ -5,35 +5,30 @@ import holoviews as hv
 from bokeh.models import HoverTool
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-import ast
 
 # Configurar Panel y Holoviews
 pn.extension('tabulator', 'bokeh')
 hv.extension('bokeh')
 
 # Cargar el archivo XLS en un DataFrame con manejo de errores
-file_path_tiempo = 'articulos_eltiempo.xlsx'
-file_path_elespectador = 'articulos_elespectador.xlsx'
+file_path = 'articulos_eltiempo.xlsx'
 try:
-    df_tiempo = pd.read_excel(file_path_tiempo)
-    df_elespectador = pd.read_excel(file_path_elespectador)
+    df = pd.read_excel(file_path)
 except FileNotFoundError:
-    df_tiempo = pd.DataFrame(columns=['title', 'date', 'url', 'content', '#coments', 'coments', 'tags'])
-    df_elespectador = pd.DataFrame(columns=['title', 'date', 'url', 'text', '#coments', 'coments', 'tags'])
-    print("Uno de los archivos no fue encontrado. Por favor, verifica la ruta de los archivos.")
+    df = pd.DataFrame(columns=['title', 'date', 'url', 'content', '#coments', 'coments', 'tags'])
+    print("El archivo no fue encontrado. Por favor, verifica la ruta del archivo.")
 
 # Asegurarse de que las columnas tienen los nombres correctos
-df_tiempo.columns = ['title', 'date', 'url', 'content', '#coments', 'coments', 'tags']
-df_elespectador.columns = ['title', 'date', 'url', 'text', '#coments', 'coments', 'tags']
+df.columns = ['title', 'date', 'url', 'content', '#coments', 'coments', 'tags']
 
-# Diccionario de meses en español a números para El Tiempo
+# Diccionario de meses en español a números
 months = {
     'enero': '01', 'febrero': '02', 'marzo': '03', 'abril': '04', 'mayo': '05', 'junio': '06',
     'julio': '07', 'agosto': '08', 'septiembre': '09', 'octubre': '10', 'noviembre': '11', 'diciembre': '12'
 }
 
-# Función para convertir la fecha de texto a datetime para El Tiempo
-def convert_date_tiempo(date_str):
+# Función para convertir la fecha de texto a datetime
+def convert_date(date_str):
     try:
         parts = date_str.split()
         day = parts[0]
@@ -43,27 +38,16 @@ def convert_date_tiempo(date_str):
     except (ValueError, IndexError, KeyError):
         return None
 
-# Función para convertir la fecha de texto a datetime para El Espectador
-def convert_date_elespectador(date_str):
-    try:
-        return datetime.strptime(date_str.split('T')[0], "%Y-%m-%d")
-    except (ValueError, IndexError):
-        return None
-
 # Aplicar la conversión a la columna 'date'
-df_tiempo['date'] = df_tiempo['date'].apply(convert_date_tiempo)
-df_elespectador['date'] = df_elespectador['date'].apply(convert_date_elespectador)
-
-# Combinar los DataFrames
-df_combined = pd.concat([df_tiempo, df_elespectador], ignore_index=True)
+df['date'] = df['date'].apply(convert_date)
 
 # Función para crear los widgets de filtro
 def create_filter_widgets():
     date_range_slider = pn.widgets.DateRangeSlider(
         name='Rango:', 
-        start=df_combined['date'].min().date(), 
-        end=df_combined['date'].max().date(), 
-        value=(df_combined['date'].min().date(), df_combined['date'].max().date()), 
+        start=df['date'].min().date(), 
+        end=df['date'].max().date(), 
+        value=(df['date'].min().date(), df['date'].max().date()), 
         width=200
     )
     keyword_input = pn.widgets.TextInput(name='Palabra Clave', width=200)
@@ -74,7 +58,7 @@ def create_filter_widgets():
 def filter_news(date_range_slider, keyword_input):
     @pn.depends(date_range_slider.param.value, keyword_input.param.value)
     def filter_func(date_range, keyword):
-        filtered_df = df_combined.copy()
+        filtered_df = df.copy()
         start_date, end_date = date_range
         if (start_date and end_date) and not pd.isnull(start_date) and not pd.isnull(end_date):
             start_date = pd.to_datetime(start_date)
@@ -92,7 +76,7 @@ def create_dashboard_tab():
     
     @pn.depends(reset_button.param.clicks, watch=True)
     def reset_filters(clicks):
-        date_range_slider.value = (df_combined['date'].min().date(), df_combined['date'].max().date())
+        date_range_slider.value = (df['date'].min().date(), df['date'].max().date())
         keyword_input.value = ''
     
     filtered_news = pn.widgets.Tabulator(filter_news(date_range_slider, keyword_input), height=580, sizing_mode='stretch_width')
@@ -104,44 +88,36 @@ def create_dashboard_tab():
 
 # Crear layout para la comparación de métricas
 def create_comparison_tab():
-    start_date_range_1 = pn.widgets.DateRangeSlider(name='Rango de Fecha 1:', start=df_combined['date'].min().date(), end=df_combined['date'].max().date(), value=(df_combined['date'].min().date(), df_combined['date'].max().date()), width=300)
-    start_date_range_2 = pn.widgets.DateRangeSlider(name='Rango de Fecha 2:', start=df_combined['date'].min().date(), end=df_combined['date'].max().date(), value=(df_combined['date'].min().date(), df_combined['date'].max().date()), width=300)
-
-        # Función para convertir una cadena de texto en una lista de etiquetas
-    def safe_eval(tags_str):
-        try:
-            return ast.literal_eval(tags_str)
-        except (ValueError, SyntaxError):
-            return []
-
-    # En la función compare_ranges, usa safe_eval en lugar de eval
+    start_date_range_1 = pn.widgets.DateRangeSlider(name='Rango de Fecha 1:', start=df['date'].min().date(), end=df['date'].max().date(), value=(df['date'].min().date(), df['date'].max().date()), width=300)
+    start_date_range_2 = pn.widgets.DateRangeSlider(name='Rango de Fecha 2:', start=df['date'].min().date(), end=df['date'].max().date(), value=(df['date'].min().date(), df['date'].max().date()), width=300)
+    
     @pn.depends(start_date_range_1.param.value, start_date_range_2.param.value)
     def compare_ranges(date_range_1, date_range_2):
         start_date_1, end_date_1 = date_range_1
         start_date_2, end_date_2 = date_range_2
-
-        range_1_df = df_combined[(df_combined['date'] >= pd.to_datetime(start_date_1)) & (df_combined['date'] <= pd.to_datetime(end_date_1))]
-        range_2_df = df_combined[(df_combined['date'] >= pd.to_datetime(start_date_2)) & (df_combined['date'] <= pd.to_datetime(end_date_2))]
-
-        range_1_tags = pd.Series([tag for tags in range_1_df['tags'] for tag in safe_eval(tags)]).value_counts().nlargest(20)
-        range_2_tags = pd.Series([tag for tags in range_2_df['tags'] for tag in safe_eval(tags)]).value_counts().nlargest(20)
-
+        
+        range_1_df = df[(df['date'] >= pd.to_datetime(start_date_1)) & (df['date'] <= pd.to_datetime(end_date_1))]
+        range_2_df = df[(df['date'] >= pd.to_datetime(start_date_2)) & (df['date'] <= pd.to_datetime(end_date_2))]
+        
+        range_1_tags = pd.Series([tag for tags in range_1_df['tags'] for tag in eval(tags)]).value_counts().nlargest(20)
+        range_2_tags = pd.Series([tag for tags in range_2_df['tags'] for tag in eval(tags)]).value_counts().nlargest(20)
+        
         comparison_df = pd.DataFrame({
             'Tags': list(set(range_1_tags.index).union(set(range_2_tags.index))),
             'Rango 1': [range_1_tags.get(tag, 0) for tag in set(range_1_tags.index).union(set(range_2_tags.index))],
             'Rango 2': [range_2_tags.get(tag, 0) for tag in set(range_1_tags.index).union(set(range_2_tags.index))]
         }).set_index('Tags')
-
+        
         bars = hv.Bars(comparison_df.reset_index(), kdims='Tags', vdims=['Rango 1', 'Rango 2']).opts(
             width=1200, height=600, tools=['hover'], xrotation=45, show_legend=True)
-
+        
         news_counts = {
             'Rango 1': len(range_1_df),
             'Rango 2': len(range_2_df)
         }
-
+        
         news_count_pane = pn.pane.Markdown(f"**Cantidad de Noticias:**\n\n- Rango 1: {news_counts['Rango 1']}\n- Rango 2: {news_counts['Rango 2']}")
-
+        
         return pn.Column(bars, news_count_pane)
     
     return pn.Column(
@@ -160,7 +136,7 @@ def create_wordcloud(file_path):
     text = ' '.join(words_df['token'])
     
     # Generar la nube de palabras
-    wordcloud = WordCloud(width=1200, height=1000, background_color='white').generate(text)
+    wordcloud = WordCloud(width=600, height=400, background_color='white').generate(text)
     
     # Mostrar la nube de palabras usando matplotlib
     plt.figure(figsize=(10, 5))
@@ -168,20 +144,67 @@ def create_wordcloud(file_path):
     plt.axis('off')
     
     # Convertir la figura de matplotlib en un objeto Panel
-    return pn.pane.Matplotlib(plt.gcf())
+    return pn.pane.Matplotlib(plt.gcf(), width=600, height=400)
 
-# Función para crear la pestaña de sentimientos
+# Función para crear la tabla interactiva de sentimientos con gráficos
 def create_sentiments_table():
-    # Cargar los datos de los archivos XLSX
+    # Cargar los datos de "sentiment_word_counts_text.xlsx"
     sentiments_df = pd.read_excel('sentiment_word_counts_text.xlsx')
     
-    # Crear una tabla interactiva para mostrar los datos
-    sentiments_table = pn.widgets.Tabulator(sentiments_df, pagination='remote', page_size=10)
-    
-    return pn.Column(
-        "# Tabla de Sentimientos",
-        sentiments_table
+    # Crear un gráfico de barras de la distribución de sentimientos
+    sentiment_counts = sentiments_df.groupby('sentiment')['count'].sum().reset_index()
+    bars = hv.Bars(sentiment_counts, kdims='sentiment', vdims='count').opts(
+        width=800, height=400, tools=[HoverTool(tooltips=[('Sentiment', '@sentiment'), ('Count', '@count')])],
+        xlabel='Sentiment', ylabel='Count', color='sentiment', show_legend=False, toolbar='above'
     )
+    
+    # Cargar los datos de "negative_words_text.xlsx"
+    negative_words_df = pd.read_excel('negative_words_text.xlsx')
+
+    # Crear un gráfico de barras para las palabras negativas más frecuentes
+    negative_words = negative_words_df.groupby(['sentiment', 'lemma'])['count'].sum().reset_index()
+    negative_words = negative_words[negative_words['count'] > 1]
+    negative_words = negative_words.sort_values(by='count', ascending=False).groupby('sentiment').head(10)
+    negative_words_bars = hv.Bars(negative_words, kdims=['sentiment', 'lemma'], vdims='count').opts(
+        width=800, height=400, tools=[HoverTool(tooltips=[('Sentiment', '@sentiment'), ('Word', '@lemma'), ('Count', '@count')])],
+        xlabel='Sentiment', ylabel='Count', color='sentiment', show_legend=False, toolbar='above', xrotation=45
+    )
+    
+    # Cargar los datos de "negative_words_text_tiempo.xlsx"
+    negative_words_df_et = pd.read_excel('negative_words_text_tiempo.xlsx')
+
+    # Crear un gráfico de barras para las palabras negativas más frecuentes
+    negative_words_et = negative_words_df_et.groupby(['sentiment', 'lemma'])['count'].sum().reset_index()
+    negative_words_et = negative_words_et[negative_words_et['count'] > 1]
+    negative_words_et = negative_words_et.sort_values(by='count', ascending=False).groupby('sentiment').head(10)
+    negative_words_bars_et = hv.Bars(negative_words_et, kdims=['sentiment', 'lemma'], vdims='count').opts(
+        width=800, height=400, tools=[HoverTool(tooltips=[('Sentiment', '@sentiment'), ('Word', '@lemma'), ('Count', '@count')])],
+        xlabel='Sentiment', ylabel='Count', color='sentiment', show_legend=False, toolbar='above', xrotation=45
+    )
+
+    # Descripción del gráfico de palabras negativas
+    negative_words_description_1 = pn.pane.Markdown("""
+    ## Palabras Negativas Más Frecuentes
+    El gráfico a continuación muestra las palabras negativas más frecuentes encontradas en El Espectador.
+    """)
+    
+    # Descripción del gráfico de palabras negativas
+    negative_words_description_2 = pn.pane.Markdown("""
+    ## Palabras Negativas Más Frecuentes
+    El gráfico a continuación muestra las palabras negativas más frecuentes encontradas en El Tiempo.
+    """)
+    
+    # Combinar los gráficos en un layout
+    layout = pn.Column(
+        "# Visualización de Sentimientos",
+        bars,
+        negative_words_description_1,
+        negative_words_bars,
+        negative_words_description_2,
+        negative_words_bars_et
+    )
+    
+    return layout
 
 # Función para crear la pestaña de comentarios
 def create_comments_tab():
